c57ffb73dd999eac4e6cad8e779299018c43eada
leojaimesson <leo.jaimesson.dev@gmail.com>
2017-08-25 11:56:08 -0300

    class utilitaria para receber upload de arquivos implementado

---
+++ src/util/UploadFile.ts
@@ -0,0 +1,15 @@
+export class UploadFile {
+    
+    private file;
+
+    constructor() {
+        this.file = new FileReader();
+    }
+
+    read(file, events):void {
+        events.forEach((event) => {
+            this.file.addEventListener(event.event, event.callback.bind(this.file));
+        });
+        this.file.readAsText(file);
+    }
+}
