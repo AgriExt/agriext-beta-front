91810a092d2af6befa124e0ca0c315dec724d9c0
leojaimesson <leo.jaimesson.dev@gmail.com>
2017-09-27 20:20:01 -0300

    up

--- src/app/generate-model/generate-model.component.ts
+++ src/app/generate-model/generate-model.component.ts
@@ -19,15 +19,14 @@ export class GenerateModelComponent implements OnInit {
   gerar(type) {
     this.restService.gerarModelo(sessionStorage.getItem('csv-name'), sessionStorage.getItem('csv-body'), type).subscribe((data) => {
       let dataSplit = data.split('smashline');
-      console.log(data);
+      // console.log(data);
       this.avaliacao = dataSplit[0];
       this.modelo = dataSplit[1];
     });
   }
 
   getURL() {
-    return "http://200.129.38.177:8080/data/download/" + sessionStorage.getItem('csv-name').replace('.csv','');
-    // return "http://200.129.38.177:8080/data/download/" + sessionStorage.getItem('csv-name');
+    return "http://10.42.0.1:8080/data/download/" + sessionStorage.getItem('csv-name')+'.model';
   }
 
 }
